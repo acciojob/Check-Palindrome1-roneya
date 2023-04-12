71abe3aec8db6bd5ea2eae29b1929da7817027ce
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-12 13:36:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+	for(let i=0;i<str.length/2;i++){
+		if(str.charAt(i)!=str.charAt(str.length-1-i))
+			return false;
+	}
+	return true;
 
 }
 module.exports = palindrome
