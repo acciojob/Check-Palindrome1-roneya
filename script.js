2207194f3d97906f7a8ecd550e5fc6572adeba81
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-12 13:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // complete the given function
 
 function palindrome(str){
+	str = str.toUpperCase();
 	for(let i=0;i<str.length/2;i++){
 		if(str.charAt(i)!=str.charAt(str.length-1-i))
 			return false;
